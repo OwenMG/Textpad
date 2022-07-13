30b562a705afde430c00d0af3b94f63e7011f934
OwenMG <36777755+OwenMG@users.noreply.github.com>
2022-07-12 17:34:01 -0700

    begins webpack configuration

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,6 +18,14 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js'
+      })
       
     ],
 
